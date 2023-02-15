3f48a06d79d1aa2b39a2aa8e6654638526a3b70d
Damien-Garcia-Bioinformatics <damien.garcia@laposte.net>
2023-02-15 12:54:50 +0100

    Initialization process is working, but one error s '<' not supported between instances of 'NoneType' and 'float' persist. Probably a file access error

--- pipeline.py
+++ pipeline.py
@@ -8,7 +8,6 @@
 # To do to dooo :
 # Dans le rapport, ajouter les 5 meilleures séquences produites.
 # Modélisation moléculaire d'au moins un séquence.
-# Utiliser un générateur de nom aléatoire pour paralléliser sur des fichiers différents (forsa)
 
 
 # ------------------------------ Library imports ----------------------------- #
@@ -27,6 +26,11 @@ from joblib import Parallel, delayed    # Parallelization process
 from tqdm import tqdm                   # Progress Bar
 from tqdm_joblib import tqdm_joblib     # Progress Bar for parallel execution
 
+
+
+# ---------------------------------------------------------------------------- #
+#                                   Functions                                  #
+# ---------------------------------------------------------------------------- #
 
 # ------------------------------- Miscellaneous ------------------------------ #
 
@@ -101,23 +105,52 @@ def write_csv(path, batch):
 # ------------------------------ Initialization ------------------------------ #
 
 
-def initialization(path, pdbFile):
-    baseName = os.path.basename(baseName)
+def initialization(path_progs, basename):
+    print("Initialization process")
+
     # Running PDB to AA program
-    subprocess.run(f"./{path['pdb2aa']} {pdbFile} {baseName}.aaseq",
+    print("Running PDB to AA program")
+    subprocess.run(f"perl {path_progs['pdb2aa']} {basename}.pdb ./results_{basename}/data/",
                    shell=True)
+    length = 0
+    with open(f"results_{basename}/data/{basename}.pdb.aa", "r+") as file:
+        seq = file.read()
+        file.seek(0)
+        file.write(f">{basename}\n{seq}")
+        length = len(seq)
     
     # Running DSSP program
-    subprocess.run(f"./{path['dssp']} -i {pdbFile} -o {baseName}.dssp",
+    print("Running DSSP program")
+    subprocess.run(f"./{path_progs['dssp']} -i {basename}.pdb -o ./results_{basename}/data/{basename}.dssp",
                    shell=True)
     
     # Running DSSP_SEPARATOR program
-    subprocess.run(f"perl {path['dssp_sep']} {baseName}.dssp .",
+    print("Running DSSP_SEPARATOR program")
+    subprocess.run(f"perl {path_progs['dssp_sep']} results_{basename}/data/{basename}.dssp ./results_{basename}/data/",
                    shell=True)
     
     # Running DSSP to PB program
-    subprocess.run(f"perl {path['dssp2pb']} {baseName}_A.dssp",
+    print("Running DSSP to PB program")
+    subprocess.run(f"perl {path_progs['dssp2pb']} results_{basename}/data/{basename}_A.dssp ./results_{basename}/data/",
                    shell=True)
+    with open(f"results_{basename}/data/{basename}_A.dssp.pb", "r+") as file:
+        seq = file.read()
+        file.seek(0)
+        file.write(f">{basename}\n{seq}")
+
+    # Running FORSA
+    print("Running FORSA")
+    subprocess.run(f"./{path_progs['forsa']} results_{basename}/data/{basename}.pdb.aa results_{basename}/data/{basename}_A.dssp.pb -5 > results_{basename}/data/{basename}.forsa",
+                   shell=True)
+    objective = extract_zscore(f"results_{basename}/data/{basename}.forsa")
+
+    print("Selected parameters are:")
+    print(f"  - Batch size = 100")
+    print(f"  - Seqeuences length = {length}")
+    print(f"  - Selection threshold = {objective/2}")
+    print(f"  - zScore objective = {objective}\n")
+
+    return length, objective
 
 
 # --------------------------- Histogram generation --------------------------- #
@@ -169,35 +202,38 @@ def hist_diff(pool, scores1, scores2):
 # ---------------------------- Initial generation ---------------------------- #
 
 
-def init_generation():
+# Parallelized process of generating sequence until initial validation of minimal score.
+# Function called by init_process()
+def init_generation(threshold, length):
     seq = ""
     zScore = 0.0
     id = uuid.uuid4()
-    while zScore < 3.0:      # Initial sequence validation threshold is set to 3
+    while zScore < threshold:      # Initial sequence validation threshold is set to 3
         # Sequence generation
         seq = sequence_generator(length)
-        write_sequence(seq, f"temp/{id}.aaseq")
+        write_sequence(seq, f"results_{basename}/temp/{id}.aaseq")
         
         # Forsa algorithm execution
         subprocess.run(
-            f"./{path['forsa']} temp/{id}.aaseq {path['pbseq']} -5 > temp/{id}.forsa",
+            f"./{path_progs['forsa']} results_{basename}/temp/{id}.aaseq results_{basename}/data/{basename}.dssp.pb -5 > results_{basename}/temp/{id}.forsa",
             shell=True
         )
-        zScore = extract_zscore(f"temp/{id}.forsa")
+        zScore = extract_zscore(f"results_{basename}/temp/{id}.forsa")
     
     # Cleaning temporary files
-    os.remove(f"temp/{id}.aaseq")
-    os.remove(f"temp/{id}.forsa")
+    os.remove(f"results_{basename}/temp/{id}.aaseq")
+    os.remove(f"results_{basename}/temp/{id}.forsa")
 
     return zScore, seq
 
 
-def init_process(pool, initRun):
+# First process of sequences generation
+def init_process(pool, initRun, threshold, length):
     # Parallelized generation and scoring of sequences until a valid pool of 100 sequences is obtained
     print("Generation of pool of sequences:")
     data = []
     with tqdm_joblib(desc="", total=pool) as progress_bar:
-        data += Parallel(n_jobs=-1)(delayed(init_generation)() for i in range(pool))
+        data += Parallel(n_jobs=-1)(delayed(init_generation)(threshold, length) for i in range(pool))
     
     for result in data:
         initRun.loc[len(initRun.index)] = [result[0], result[1]]
@@ -212,22 +248,25 @@ def init_process(pool, initRun):
 # ----------------------------- Second generation ---------------------------- #
 
 
+# Parallelized process of scoring sequences in all but first mutation/scoring cycles.
+# Function called by following_processes().
 def following_generation(seq, seqN, nbCopies):
     id = uuid.uuid4()
-    write_sequence(seq, f"temp/{id}.aaseq")
+    write_sequence(seq, f"results_{basename}/temp/{id}.aaseq")
     subprocess.run(
-        f"./{path['forsa']} temp/{id}.aaseq {path['pbseq']} -5 > temp/{id}.forsa",
+        f"./{path_progs['forsa']} results_{basename}/temp/{id}.aaseq results_{basename}/data/{basename}.dssp.pb -5 > results_{basename}/temp/{id}.forsa",
         shell=True
     )
     oriSeq = seqN//nbCopies
-    zScore = extract_zscore(f"temp/{id}.forsa")
+    zScore = extract_zscore(f"results_{basename}/temp/{id}.forsa")
 
-    os.remove(f"temp/{id}.aaseq")
-    os.remove(f"temp/{id}.forsa")
+    os.remove(f"results_{basename}/temp/{id}.aaseq")
+    os.remove(f"results_{basename}/temp/{id}.forsa")
     
     return oriSeq, zScore, seq
 
 
+# All but first mutation/scoring cycles (Cycles 1 and over).
 def following_processes(runIter, sequences, newRun):
     batch = []
     nbCopies = 100
@@ -257,36 +296,50 @@ def following_processes(runIter, sequences, newRun):
     return newRun
     
 
-# ----------------------------------- Main ----------------------------------- #
 
+# ---------------------------------------------------------------------------- #
+#                                     Main                                     #
+# ---------------------------------------------------------------------------- #
 
 if __name__ == "__main__":
-    # Dictionary structure containing all necessary paths
-    path = {
-        "forsa": "forsa/forsa_global",
-        "dssp": "dssp/dssp-2.0.4-linux-i386",
+    # Checking command line arguments
+    # Structure of interest should be one chain only or the first chain of the pdb file.
+    basename = ""
+    if len(sys.argv) != 2 or not sys.argv[1].endswith(".pdb"):
+        help()
+        exit(1)
+    else:
+        basename = os.path.basename(sys.argv[1])[:-4]
+
+    # Dictionary structures containing all necessary paths
+    path_progs = {
+        "forsa":    "forsa/forsa_global",
+        "dssp":     "dssp/dssp-2.0.4-linux-i386",
         "dssp_sep": "dssp/dssp_separator.pl",
-        "dssp2pb": "dssp/dssp_to_pb_tor_rmsda.pl",
-        "pdb2aa": "dssp/pdb_to_aa.pl",
-
-        "pbseq": "2xiw.pb",
+        "dssp2pb":  "dssp/dssp_to_pb_tor_rmsda.pl",
+        "pdb2aa":   "dssp/pdb_to_aa.pl",
     }
-
-    # # Checking command line arguments
-    # doRunInit = False
-    # for args in sys.argv:
-    #     if args.endswith('.pb'):
-    #         doRunInit = True
-    #         path['pbseq'] = args
+    # path_files = {
+    #     "pdb":   f"{basename}.pdb",
+    #     "aaseq": f"results_{basename}/data/{basename}.pdb.aa",
+    #     "dssp":  f"results_{basename}/data/{basename}.dssp",
+    #     "pbseq": f"results_{basename}/data/{basename}.pbseq",
+    #     "forsa": f"results_{basename}/data/{basename}_A.forsa",
+    #     "temp":  f"results_{basename}/temp",
+    # }
 
     # Checking for files and directories existence
-    if not os.path.exists(path['forsa'] or not os.path.exists(path['pbseq'])):
+    if not os.path.exists(path_progs['forsa']):
         print("[Err1] Missing required files to execute generation.")
         exit(1)
-    if not os.path.exists("temp"):
-        os.makedirs("temp")
-    if not os.path.exists("results"):
-        os.makedirs("results")
+    if not os.path.exists(f"results_{basename}"):
+        os.makedirs(f"results_{basename}")
+        os.makedirs(f"results_{basename}/data")
+        os.makedirs(f"results_{basename}/temp")
+    elif not os.path.exists(f"results_{basename}/data"):
+        os.makedirs(f"results_{basename}/data")
+    if not os.path.exists(f"results_{basename}/temp"):
+        os.makedirs(f"results_{basename}/temp")
 
     # Data types storing generated sequences and corresponding zScores
     initRun = pd.DataFrame({"zScore": pd.Series(dtype='float'),
@@ -294,20 +347,20 @@ if __name__ == "__main__":
     newRun  = pd.DataFrame({"oriSeq": pd.Series(dtype='int'),
                             "zScore": pd.Series(dtype='float'),
                             "sequence": pd.Series(dtype='str')})
-
-    # Generation constants
-    length = 64         # Len of Sac7d protein used as reference for sequence generation.
-    pool = 100          # Set to 100 but can be lowered for testing purposes.
-    objective = 6.44    # zScore of reference for Sac7d 2XIW protein.
-
+    
     # Print header
     header()
+
+    # Generation constants
+    pool = 100
+    length, objective = initialization(path_progs, basename)
+    threshold = objective/2
 
     # Generation and mutation cycles
     runIter, bestScore = 0, 0
     while runIter < 3 or bestScore < objective :
         if runIter == 0:
-            init_process(pool, initRun)
+            init_process(pool, initRun, threshold, length)
         elif runIter == 1:
             sequences = initRun['sequence'].to_list()
             newRun = following_processes(runIter, sequences, newRun)
