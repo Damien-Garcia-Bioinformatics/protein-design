eb84ea45b22d6de12da64407d58137ecf122cb80
Damien-Garcia-Bioinformatics <damien.garcia@laposte.net>
2023-02-16 20:45:36 +0100

    .

--- pipeline.py
+++ pipeline.py
@@ -3,11 +3,29 @@
 
 # Protein Block Design, Master's degree (2nd year) - Structural Bioinformatics project
 # Author : Damien GARCIA, M2BB
-
-
-# To do to dooo :
-# Dans le rapport, ajouter les 5 meilleures séquences produites.
-# Modélisation moléculaire d'au moins un séquence.
+#
+# This script is separated in 3 sections :
+#   -   Initilization process :
+#       The PDB file on command line argument is treated in order to generate the necessary
+#       files (Amino acids sequence, PB sequence, forsa initial zScore).
+#       Be aware that this method only works if you aim to generate potential homologues
+#       for the chain A of the structure. If you wan't to work on any other chain, modify
+#       the PDB file accordingly.
+#   -   Generation cycle :
+#       Sequences are generated following the length of the reference structure. The
+#       generation follows a uniform law for the amino acid selection at each position to
+#       avoid any a priori. Each sequence is scored using FORSA algorithm and is selected
+#       only if the threshold (half the reference sequence zScore) is exceeded. This process
+#       is repeated until a batch of 100 sequence is obtained.
+#   -   Mutation cycle(s) :
+#       Each sequence of the batch is cloned a 1000 times and mutated randomly with a
+#       mutation rate set to 20%. All newly mutated sequence is scored and the top100 zScoring
+#       sequences are selected for another cycle.
+#       Cycles are repeated until either the number of repetition reach 5, or the reference
+#       zScore is reached or exceeded by at least one sequence of the batch.
+#
+# Results are stored in csv files for the zScore and the corresponding sequences. A histogram
+# graph of the zScore repartition of each batch of sequence is also provided.
 
 
 # ------------------------------ Library imports ----------------------------- #
@@ -15,6 +33,7 @@
 
 import os                               # File handling
 import sys                              # Command line arguments
+import time                             # Script execution elapsed time
 
 import subprocess                       # Forsa and dssp algorithms execution
 import random                           # Sequence generation and mutation rate
@@ -98,19 +117,15 @@ def extract_zscore(path):
 
 # Writes zScore and sequence from selected pool of sequence to csv file.
 def write_csv(path, batch):
-    print(f"Writing selected pool of sequences to '{path}'")
+    print(f"\nWriting selected pool of sequences to '{path}'")
     batch.to_csv(path, sep=';', encoding='utf-8', index=False)
 
 
 # --------------------------- Histogram generation --------------------------- #
 
 
-def hist_init():
-    return 0
-
-
-def hist_generation(runIter, scores):
-    print(f"Histogram generated to 'gen{runIter}_hist.png'")
+def hist_generation(runIter, scores, bestScore):
+    print(f"\nHistogram generated to 'gen{runIter}_hist.png'")
     if isinstance(scores, list):
         x = scores
     else:
@@ -118,70 +133,47 @@ def hist_generation(runIter, scores):
     plt.hist(x, density=True, bins=100)
     plt.xlabel('zScore')
     plt.ylabel('Percentage')
-    plt.axvline(3.0, color='r', linestyle='--', label='...')
+    if bestScore != 0:
+        plt.axvline(bestScore, color='r', linestyle='--', label='Highest scoring sequence of previous cycle')
     plt.title(f'Generation{runIter} zScore distribution')
     plt.savefig(f'results_{basename}/gen{runIter}.png', dpi='figure', format='png')
     plt.clf()
 
 
-### WIP ###
-def hist_diff(pool, scores1, scores2):
-    nbCopies = 100
-    s1 = scores1['zScore'].to_list()
-    s2 = scores2['zScore'].to_list()
-
-    result = []
-    for i in range(len(s1)):
-        count = 0
-        for j in range(100+i*100):
-            if s2[j] > s1[i]:
-                count += 1
-        result.append((count/100)*100)
-        count
-    # plt.bar(result)
-    # plt.xlabel("Parent sequence ID")
-    # plt.ylabel("Percentage")
-    # plt.title("Percentage of sequence with higher zScore than their respective parent")
-    # plt.show()
-    # plt.savefig
-    # plt.clf()
-### WIP ###
-
-
 # ------------------------------ Initialization ------------------------------ #
 
 
-def initialization(path_progs, basename):
+def initialization(path, basename):
     # Running PDB to AA program
-    subprocess.run(f"perl {path_progs['pdb2aa']} {basename}.pdb ./results_{basename}/data/",
+    subprocess.run(f"perl {path['pdb2aa']} {basename}.pdb ./{path['data']}",
                    shell=True)
     length = 0
-    with open(f"results_{basename}/data/{basename}.pdb.aa", "r+") as file:
+    with open(f"{path['data']}/{basename}.pdb.aa", "r+") as file:
         seq = file.read()
         file.seek(0)
         file.write(f">{basename}\n{seq}")
         length = len(seq)
     
     # Running DSSP program
-    subprocess.run(f"./{path_progs['dssp']} -i {basename}.pdb -o ./results_{basename}/data/{basename}.dssp",
+    subprocess.run(f"./{path['dssp']} -i {basename}.pdb -o ./{path['data']}/{basename}.dssp",
                    shell=True)
     
     # Running DSSP_SEPARATOR program
-    subprocess.run(f"perl {path_progs['dssp_sep']} results_{basename}/data/{basename}.dssp ./results_{basename}/data/",
+    subprocess.run(f"perl {path['dssp_sep']} {path['data']}/{basename}.dssp ./{path['data']}",
                    shell=True)
     
     # Running DSSP to PB program
-    subprocess.run(f"perl {path_progs['dssp2pb']} results_{basename}/data/{basename}_A.dssp ./results_{basename}/data/",
+    subprocess.run(f"perl {path['dssp2pb']} {path['data']}/{basename}_A.dssp ./{path['data']}",
                    shell=True)
-    with open(f"results_{basename}/data/{basename}_A.dssp.pb", "r+") as file:
+    with open(f"{path['data']}/{basename}_A.dssp.pb", "r+") as file:
         seq = file.read()
         file.seek(0)
         file.write(f">{basename}\n{seq}")
 
     # Running FORSA
-    subprocess.run(f"./{path_progs['forsa']} results_{basename}/data/{basename}.pdb.aa results_{basename}/data/{basename}_A.dssp.pb -5 > results_{basename}/data/{basename}.forsa",
+    subprocess.run(f"./{path['forsa']} {path['data']}/{basename}.pdb.aa {path['data']}/{basename}_A.dssp.pb -5 > {path['data']}/{basename}.forsa",
                    shell=True)
-    objective = extract_zscore(f"results_{basename}/data/{basename}.forsa")
+    objective = extract_zscore(f"{path['data']}/{basename}.forsa")
 
     print("Selected parameters are:")
     print(f"  - Batch size = 100")
@@ -197,45 +189,47 @@ def initialization(path_progs, basename):
 
 # Parallelized process of generating sequence until initial validation of minimal score.
 # Function called by init_process()
-def init_generation(threshold, length):
+def init_generation(path, basename, threshold, length):
     seq = ""
     zScore = 0.0
     id = uuid.uuid4()
     while zScore < threshold:      # Initial sequence validation threshold is set to 3
         # Sequence generation
         seq = sequence_generator(length)
-        write_sequence(seq, f"results_{basename}/temp/{id}.aaseq")
+        write_sequence(seq, f"{path['temp']}/{id}.aaseq")
         
         # Forsa algorithm execution
         subprocess.run(
-            f"./{path_progs['forsa']} results_{basename}/temp/{id}.aaseq results_{basename}/data/{basename}_A.dssp.pb -5 > results_{basename}/temp/{id}.forsa",
+            f"./{path['forsa']} {path['temp']}/{id}.aaseq {path['data']}/{basename}_A.dssp.pb -5 > {path['temp']}/{id}.forsa",
             shell=True
         )
-        zScore = extract_zscore(f"results_{basename}/temp/{id}.forsa")
+        zScore = extract_zscore(f"{path['temp']}/{id}.forsa")
     
     # Cleaning temporary files
-    os.remove(f"results_{basename}/temp/{id}.aaseq")
-    os.remove(f"results_{basename}/temp/{id}.forsa")
+    os.remove(f"{path['temp']}/{id}.aaseq")
+    os.remove(f"{path['temp']}/{id}.forsa")
 
     return zScore, seq
 
 
 # First process of sequences generation
-def init_process(pool, initRun, threshold, length):
+def init_process(path, basename, initRun, pool, threshold, length, bestScore):
     # Parallelized generation and scoring of sequences until a valid pool of 100 sequences is obtained
     print("Generation of pool of sequences:")
     data = []
     with tqdm_joblib(desc="", total=pool) as progress_bar:
-        data += Parallel(n_jobs=-1)(delayed(init_generation)(threshold, length) for i in range(pool))
+        data += Parallel(n_jobs=-1)(delayed(init_generation)(path, basename, threshold, length) for i in range(pool))
     
     for result in data:
         initRun.loc[len(initRun.index)] = [result[0], result[1]]
     
-    print("\nTop10 scoring sequences from generation:")
+    print("\nInitial sequences generated:")
     print(initRun.sort_values(by=["zScore"], ascending=False))
     
-    # hist_generation(runIter, allScores)
-    write_csv(f"results_{basename}/gen0_data.csv", initRun)
+    hist_generation(runIter, initRun, bestScore)
+    write_csv(f"{path['results']}/gen0.csv", initRun)
+
+    return initRun
 
 
 # ----------------------------- Second generation ---------------------------- #
@@ -243,31 +237,31 @@ def init_process(pool, initRun, threshold, length):
 
 # Parallelized process of scoring sequences in all but first mutation/scoring cycles.
 # Function called by following_processes().
-def following_generation(seq, seqN, nbCopies):
+def following_generation(path, basename, seq, seqN, nbCopies):
     id = uuid.uuid4()
-    write_sequence(seq, f"results_{basename}/temp/{id}.aaseq")
+    write_sequence(seq, f"{path['temp']}/{id}.aaseq")
     subprocess.run(
-        f"./{path_progs['forsa']} results_{basename}/temp/{id}.aaseq results_{basename}/data/{basename}_A.dssp.pb -5 > results_{basename}/temp/{id}.forsa",
+        f"./{path['forsa']} {path['temp']}/{id}.aaseq {path['data']}/{basename}_A.dssp.pb -5 > {path['temp']}/{id}.forsa",
         shell=True
     )
     oriSeq = seqN//nbCopies
-    zScore = extract_zscore(f"results_{basename}/temp/{id}.forsa")
+    zScore = extract_zscore(f"{path['temp']}/{id}.forsa")
 
-    os.remove(f"results_{basename}/temp/{id}.aaseq")
-    os.remove(f"results_{basename}/temp/{id}.forsa")
+    os.remove(f"{path['temp']}/{id}.aaseq")
+    os.remove(f"{path['temp']}/{id}.forsa")
     
     return oriSeq, zScore, seq
 
 
 # All but first mutation/scoring cycles (Cycles 1 and over).
-def following_processes(runIter, sequences, newRun):
+def following_processes(path, basename, newRun, runIter, sequences, bestScore):
     batch = []
     nbCopies = 100
 
     # Mutating sequences
     print("\nMutating sequences:")
     for i in tqdm(range(len(sequences))):
-        # Generation of 1000 mutated copies for each sequence
+        # Generation of nbCopies mutated copies for each sequence
         batch.append(Parallel(n_jobs=-1)(delayed(mutate)(sequences[i]) for j in range(nbCopies)))
     batch = flatten_list(batch)
 
@@ -275,16 +269,15 @@ def following_processes(runIter, sequences, newRun):
     print("\nForsa zScore calculation:")
     data = []
     with tqdm_joblib(desc="", total=len(batch)) as progress_bar:
-        data.append(Parallel(n_jobs=-1)(delayed(following_generation)(batch[i], i, nbCopies) for i in range(len(batch))))
+        data.append(Parallel(n_jobs=-1)(delayed(following_generation)(path, basename, batch[i], i, nbCopies) for i in range(len(batch))))
     data = flatten_list(data)
 
     for result in data:
         newRun.loc[len(newRun.index)] = [result[0], result[1], result[2]]
     
-    hist_generation(runIter, newRun)
+    hist_generation(runIter, newRun, bestScore)
     newRun = newRun.nlargest(100, 'zScore')
-    # print(newRun)
-    write_csv(f"results_{basename}/gen{runIter}_data.csv", newRun)
+    write_csv(f"{path['results']}/gen{runIter}.csv", newRun)
 
     return newRun
     
@@ -295,6 +288,9 @@ def following_processes(runIter, sequences, newRun):
 # ---------------------------------------------------------------------------- #
 
 if __name__ == "__main__":
+    # Start timer
+    start = time.time()
+
     # Checking command line arguments
     # Structure of interest should be one chain only or the first chain of the pdb file.
     basename = ""
@@ -305,39 +301,30 @@ if __name__ == "__main__":
         basename = os.path.basename(sys.argv[1])[:-4]
 
     # Dictionary structures containing all necessary paths
-    path_progs = {
-        "forsa":    "forsa/forsa_global",
-        "dssp":     "dssp/dssp-2.0.4-linux-i386",
+    path = {
+        "forsa": "forsa/forsa_global",
+        "dssp": "dssp/dssp-2.0.4-linux-i386",
         "dssp_sep": "dssp/dssp_separator.pl",
-        "dssp2pb":  "dssp/dssp_to_pb_tor_rmsda.pl",
-        "pdb2aa":   "dssp/pdb_to_aa.pl",
-    }
-    # dir_path = {
-    #     "data": f"results_{basename}/data/",
-    #     "temp": f"results_{basename}/temp/",
-    # }
+        "dssp2pb": "dssp/dssp_to_pb_tor_rmsda.pl",
+        "pdb2aa": "dssp/pdb_to_aa.pl",
 
-    # path_files = {
-    #     "pdb":   f"{basename}.pdb",
-    #     "aaseq": f"results_{basename}/data/{basename}.pdb.aa",
-    #     "dssp":  f"results_{basename}/data/{basename}.dssp",
-    #     "pbseq": f"results_{basename}/data/{basename}.pbseq",
-    #     "forsa": f"results_{basename}/data/{basename}_A.forsa",
-    #     "temp":  f"results_{basename}/temp",
-    # }
+        "results": f"results_{basename}",
+        "data": f"results_{basename}/data",
+        "temp": f"results_{basename}/temp",
+    }
 
     # Checking for files and directories existence
-    if not os.path.exists(path_progs['forsa']):
+    if not os.path.exists(path['forsa']):
         print("[Err1] Missing required files to execute generation.")
         exit(1)
     if not os.path.exists(f"results_{basename}"):
-        os.makedirs(f"results_{basename}")
-        os.makedirs(f"results_{basename}/data")
-        os.makedirs(f"results_{basename}/temp")
-    elif not os.path.exists(f"results_{basename}/data"):
-        os.makedirs(f"results_{basename}/data")
-    if not os.path.exists(f"results_{basename}/temp"):
-        os.makedirs(f"results_{basename}/temp")
+        os.makedirs(path['results'])
+        os.makedirs(path['data'])
+        os.makedirs(path['temp'])
+    elif not os.path.exists(path['data']):
+        os.makedirs(path['data'])
+    if not os.path.exists(path['temp']):
+        os.makedirs(path['temp'])
 
     # Data types storing generated sequences and corresponding zScores
     initRun = pd.DataFrame({"zScore": pd.Series(dtype='float'),
@@ -351,23 +338,34 @@ if __name__ == "__main__":
 
     # Generation constants
     pool = 100
-    length, objective = initialization(path_progs, basename)
+    length, objective = initialization(path, basename)
     threshold = objective/2
 
     # Generation and mutation cycles
     runIter, bestScore = 0, 0
-    while runIter < 3 or bestScore < objective :
+    while bestScore < objective and runIter < 5:
         if runIter == 0:
-            init_process(pool, initRun, threshold, length)
+            initRun = init_process(path, basename, initRun, pool, threshold, length, bestScore)
+            bestScore = max(initRun['zScore'].to_list())
         elif runIter == 1:
             sequences = initRun['sequence'].to_list()
-            newRun = following_processes(runIter, sequences, newRun)
+            newRun = following_processes(path, basename, newRun, runIter, sequences, bestScore)
             bestScore = max(initRun['zScore'].to_list())
             print(newRun)
         else:
             sequences = newRun['sequence'].to_list()
-            newRun = following_processes(runIter, sequences, newRun)
+            newRun = following_processes(path, basename, newRun, runIter, sequences, bestScore)
             bestScore = max(newRun['zScore'].to_list())
             print(newRun)
-        print(bestScore)
         runIter += 1
+    
+    os.rmdir(path['temp'])
+    
+    # End timer
+    end = time.time()
+    elapsed = end - start
+    
+    print("\nScript execution results:")
+    print(f"  - Total number of cycles   = {runIter}")
+    print(f"  - Script execution time    = {round(elapsed, 2)}")
+    print(f"  - Highest scoring sequence = {bestScore}")
